eaeb04657197cfb30261c6605b184c394acb9e25
Thais <108365414+hysp0h@users.noreply.github.com>
2024-11-18 09:10:52 -0300

    Add files via upload

---
+++ PASTA MISTERIOSA/script.js
@@ -0,0 +1,22 @@
+let turno = 'X';
+let tabuleiro = ['','','','','','','','',''];
+
+function jogar (index){
+    if (tabuleiro[index]) return;
+    tabuleiro[index] = turno;
+    document.querySelectorAll('.cedula')[index].textContent = turno;
+    if (verificarVencedor()) alert(`Jogador ${turno} venceu!`);
+    turno= (turno === 'X') ? 'O' : 'X';
+}
+
+function verificarVencedor(){
+    const combinacoes = [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6]
+    ];
+    for (let [a, b ,c] of combinacoes){
+        if ( tabuleiro [a] && tabuleiro [a] === tabuleiro [b] && tabuleiro [a] === tabuleiro [c]) return true;
+    }
+    return false;
+}
